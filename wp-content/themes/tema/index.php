5ea1254e827f7988128fd2b32abf7175dc15529b
jamesraveloc <jamesraveloc52@gmail.com>
2016-06-14 09:35:04 -0500

    boostramp

--- wp-content/themes/tema/index.php
+++ wp-content/themes/tema/index.php
@@ -6,44 +6,7 @@
 
      <div class="container" class="general">
       
-     <div class="col-lg-3 col-lg-offset-8" class="derecha">
 
-       <h3>ANUNCIOS</h3>
-       
-             <div >
-        <?php rewind_posts(); ?>
-        <?php query_posts('order=Asc&cat=2') ?>
-       <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-       <!-- post -->
-       <div class="Anuncio">
-        
-        
-        <div class="foto-An">
-          <a href="<?php the_permalink(); ?>"><figure><?php if ( has_post_thumbnail() ) {
-           the_post_thumbnail(mi_tamaño_An);
-           } else { ?>
-           <img src="/wp-content/themes/tema/imagenes/wordpress-138x110.png" alt="<?php the_title(); ?>" />
-           <?php } ?>
-           <div><h1><?php the_title(); ?></h1></a>
-         </figure> 
-         
-        </div>
-        
-
-        
-
-       </div> 
-       
-
-       <?php endwhile; ?>
-       <!-- post navigation -->
-       <?php else: ?>
-       <h3>No se encontraron entradas</h3>
-       <!-- no posts found -->
-       <?php endif; ?>
-       
-       </div>
-     </div>
 
      <div class="col-lg-5" class="entradas">
 
@@ -93,6 +56,45 @@
 
                
        </div>
+
+     <div class="col-lg-3 col-lg-offset-8" class="derecha">
+
+       <h3>ANUNCIOS</h3>
+       
+             <div >
+        <?php rewind_posts(); ?>
+        <?php query_posts('order=Asc&cat=2') ?>
+       <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+       <!-- post -->
+       <div class="Anuncio">
+        
+        
+        <div class="foto-An">
+          <a href="<?php the_permalink(); ?>"><figure><?php if ( has_post_thumbnail() ) {
+           the_post_thumbnail(mi_tamaño_An);
+           } else { ?>
+           <img src="/wp-content/themes/tema/imagenes/wordpress-138x110.png" alt="<?php the_title(); ?>" />
+           <?php } ?>
+           <div><h1><?php the_title(); ?></h1></a>
+         </figure> 
+         
+        </div>
+        
+
+        
+
+       </div> 
+       
+
+       <?php endwhile; ?>
+       <!-- post navigation -->
+       <?php else: ?>
+       <h3>No se encontraron entradas</h3>
+       <!-- no posts found -->
+       <?php endif; ?>
+       
+       </div>
+     </div>
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   </body>
